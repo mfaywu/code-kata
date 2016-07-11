987df2f11847f00e34e66701ea908994fc99d8a5
Fay Wu <t-menwu@microsoft.com>
2016-07-11 15:47:53 -0700

    Initial commit

---
+++ BloomFilter/BloomFilter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Linq;
+
+namespace BloomFilterNamespace
+{
+    public class BloomFilter
+    {
+        private string[] _wordList;
+        private const int BITMAP_SIZE = 100;
+        private const int NUM_HASH_FUNCTIONS = 5;
+
+        private BitArray bitmap = new BitArray(BITMAP_SIZE);
+
+        public void AddWords(string[] wordList)
+        {
+            foreach (var word in wordList)
+            {
+                AddWord(word);
+            }
+        }
+
+        private void AddWord(string word)
+        {
+            int[] bitmapIndex = GetHashIndex(word);
+            foreach (int i in bitmapIndex)
+            {
+                bitmap[i] = true;
+            }
+        }
+
+        private int[] GetHashIndex(string word)
+        {
+            int[] bitmapIndex = new int[NUM_HASH_FUNCTIONS];
+            bitmapIndex[0] = Math.Abs(word.GetHashCode());
+
+            for (int i = 1; i < NUM_HASH_FUNCTIONS; i++)
+            {
+                bitmapIndex[i] = Math.Abs(bitmapIndex[i - 1].GetHashCode());
+            }
+
+            for (int i = 0; i < NUM_HASH_FUNCTIONS; i++)
+            {
+                bitmapIndex[i] %= BITMAP_SIZE;
+            }
+
+            return bitmapIndex;
+        }
+
+        public bool Check(string word)
+        {
+            int[] index = GetHashIndex(word);
+            foreach (int i in index)
+            {
+                if (!bitmap[i])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ UnitTestProject1/BloomFilterTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BloomFilter = BloomFilterNamespace.BloomFilter;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class BloomFilterTests
+    {
+        [TestMethod]
+        public void GIVEN_BitmapClear_WHEN_WordAdded_THEN_CheckSucceeds()
+        {
+            var bloomFilter = new BloomFilter();
+            var testWord = (new Random()).Next().ToString();
+            string[] wordList = new[] {testWord};
+            bloomFilter.AddWords(wordList);
+            Assert.IsTrue(bloomFilter.Check(wordList[0]));
+        }
+
+        [TestMethod]
+        public void GIVEN_BitmapClear_WHEN_WordMissing_THEN_CheckFails()
+        {
+            var bloomFilter = new BloomFilter();
+            string[] wordList = new[] { "second" };
+            Assert.IsFalse(bloomFilter.Check(wordList[0]));
+        }
+    }
+}
